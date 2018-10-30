5b3a56fc59cf9c3cd0319c9fb7522735d5358877
MohammedAbuhasna <mohammedhasna2015@gmail.com>
2018-10-30 23:35:43 +0300

    this the third modify

---
+++ JoksBackEnd/src/main/java/com/mohamedcoder/coder/MyEndpoint.java
@@ -0,0 +1,27 @@
+package com.mohamedcoder.coder;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+
+import com.mohamedcoder.jokes.Joke;
+
+@Api(
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.builditbigger.gradle.udacity.com",
+                ownerName = "backend.builditbigger.gradle.udacity.com",
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke() {
+        MyBean response = new MyBean();
+        response.setData(Joke.getJoke());
+        return response;
+    }
+
+}
